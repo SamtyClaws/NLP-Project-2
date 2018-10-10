b58f79e1bd5cd1a72938511e9a00ffd469dde92c
Sam Tellier <sct66@cornell.edu>
2018-10-10 16:45:20 -0400

    created project framework

---
+++ project2_fall2018/data/dev_validation_splitter.py
@@ -0,0 +1,9 @@
+# This file should be run from within the data directory.
+# Run this file with python dev_validation_splitter.py
+
+# This function takes in the name of a text file using the format specified in 
+# the project instructions, and creates new dev.txt and validation.txt files.
+def split(train_text_file):
+	# TODO: determine how to best divide the training data into dev/validation 
+	# sets, then split the train.txt file into dev.txt and validation.txt files
+	return
